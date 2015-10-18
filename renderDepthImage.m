fa3f77f36896be4611d0cb8352c8e37cd715c136
Daeyun Shin <daeyunshin@gmail.com>
2015-10-18 10:45:48 -0500

    Create renderDepthImage.m

---
+++ renderDepthImage.m
@@ -0,0 +1,4 @@
+function out = renderDepthImage(mesh, camera, imSize)
+P = camera.K*[camera.R camera.t];
+%% The mex file accepts 0-indexed triangle faces.
+out = rasterizeDepth(mesh.v, mesh.f-1, imSize, P);
